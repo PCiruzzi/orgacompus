3cc89f04c1e9b2ab804dbfac259a8eb8acdc42bf
horaciomk <hmk142@hotmail.com>
2015-09-17 02:01:57 -0300

    matriz continua y funcion proximo, valgrind tira un error pero la imagen sale bien (al igual a la del enunciado) asi q mañana me fijo bien q onda y trato de hacerlo en assembler

--- src/main.c
+++ src/main.c
@@ -5,151 +5,159 @@
 typedef enum { false, true } bool;
 
 int verificarRegla(char* argv1) {
-	char* end;
-	int regla = strtol(argv1, &end, 10);
-	if (end == argv1 || *end != '\0' || errno == ERANGE) {
-		fprintf(stderr, "El numero de regla no es valido. El mismo no contiene un valor numerico o se ha producido un overflow.\n"); //TODO: No se si es por overflow o qué
-		return -1;
-	}
-	if ((regla < 0) || (regla > 255)) {
-		fprintf(stderr, "El numero de regla no es valido. Debe ser un valor numerico entre 0 y 255.\n");
-		return -1;
-	}
-	return regla;
+  char* end;
+  int regla = strtol(argv1, &end, 10);
+  if (end == argv1 || *end != '\0' || errno == ERANGE) {
+    fprintf(stderr, "El numero de regla no es valido. El mismo no contiene un valor numerico o se ha producido un overflow.\n"); //TODO: No se si es por overflow o qué
+    return -1;
+  }
+  if ((regla < 0) || (regla > 255)) {
+    fprintf(stderr, "El numero de regla no es valido. Debe ser un valor numerico entre 0 y 255.\n");
+    return -1;
+  }
+  return regla;
 }
 
 int verificarN(char* argv2) {
-	char* end;
-	int N = strtol(argv2, &end, 10);
-	if (end == argv2 || *end != '\0' || errno == ERANGE) {
-		fprintf(stderr, "El numero de celdas e iteraciones (\"N\") no es valido. El mismo no contiene un valor numerico o se ha producido un overflow.\n"); //TODO: No se si es por overflow o qué
-		return -1;
-	}
-	if ((N < 0) || (N > 10000)) {
-		fprintf(stderr, "El numero de celdas e iteraciones (\"N\") no es valido. Debe ser un valor numerico entre 0 y 10000.\n");
-		return -1;
-	}
-	return N;
+  char* end;
+  int N = strtol(argv2, &end, 10);
+  if (end == argv2 || *end != '\0' || errno == ERANGE) {
+    fprintf(stderr, "El numero de celdas e iteraciones (\"N\") no es valido. El mismo no contiene un valor numerico o se ha producido un overflow.\n"); //TODO: No se si es por overflow o qué
+    return -1;
+  }
+  if ((N < 0) || (N > 10000)) {
+    fprintf(stderr, "El numero de celdas e iteraciones (\"N\") no es valido. Debe ser un valor numerico entre 0 y 10000.\n");
+    return -1;
+  }
+  return N;
 }
 
 FILE* verificarEntrada(char* argv3) {
-	FILE* entrada = fopen(argv3, "r");
-	if (entrada == NULL)
-		fprintf(stderr, "El archivo de entrada especificado no se ha podido abrir o no existe.\n");
-	return entrada;
+  FILE* entrada = fopen(argv3, "r");
+  if (entrada == NULL)
+    fprintf(stderr, "El archivo de entrada especificado no se ha podido abrir o no existe.\n");
+  return entrada;
 }
 
 unsigned char** crearMatriz(int N) {
-	unsigned char** matriz;
-	matriz = (unsigned char**) malloc(N * sizeof(unsigned char*));
-	int i;
-	for (i = 0; i < N; i++)
-		matriz[i] = (unsigned char*) malloc(N * sizeof(unsigned char));
-	return matriz;
+  unsigned char** matriz;
+  matriz = (unsigned char**) malloc(N * sizeof(unsigned char*));
+  int i;
+  for (i = 0; i < N; i++)
+    matriz[i] = (unsigned char*) malloc(N * sizeof(unsigned char));
+  return matriz;
 }
 
-void cargarPrimeraFila(int N, bool* continuar, FILE* entrada, unsigned char** matriz) {
-	int i = 0;
-	int aux;
-	while ((*continuar == true) && ((aux = fgetc(entrada)) != EOF)) {
-		if (aux == '0' || aux == '1') {
-			matriz[0][i] = aux - '0';
-			fprintf(stdout, "%c", matriz[0][i]);
-			fprintf(stdout, "%i\n", aux);
-			i++;
-		} else {
-			if (aux != 10){
-				fprintf(stdout, "%i\n", aux);
-				fprintf(stderr, "El formato del archivo de entrada no es correcto.\n");
-				*continuar = false;
-			} else fprintf(stdout, "fin de linea\n");
-		}
-		if (i > N) {
-			fprintf(stderr, "La cantidad de columnas en el archivo de entrada es mayor a la longitud indicada por \"N\".\n");
-			*continuar = false;
-		}
-	}
-	if ((*continuar == true) && (i < N)) {
-		fprintf(stderr, "La cantidad de columnas en el archivo de entrada es menor a la longitud indicada por \"N\".\n");
-		*continuar = false;
-	}
+void cargarPrimeraFila(int N, bool* continuar, FILE* entrada, unsigned char (*matriz)[N]) {
+  int i = 0;
+  int aux;
+  while ((*continuar == true) && ((aux = fgetc(entrada)) != EOF)) {
+    if (aux == '0' || aux == '1') {
+      matriz[0][i] = aux - '0';
+      //fprintf(stdout, "%c", matriz[0][i]);
+      //fprintf(stdout, "%i\n", aux);
+      i++;
+    } else {
+      if (aux != 10){
+        //fprintf(stdout, "%i\n", aux);
+        fprintf(stderr, "El formato del archivo de entrada no es correcto.\n");
+        *continuar = false;
+      } else fprintf(stdout, "fin de linea\n");
+    }
+    if (i > N) {
+      fprintf(stderr, "La cantidad de columnas en el archivo de entrada es mayor a la longitud indicada por \"N\".\n");
+      *continuar = false;
+    }
+  }
+  if ((*continuar == true) && (i < N)) {
+    fprintf(stderr, "La cantidad de columnas en el archivo de entrada es menor a la longitud indicada por \"N\".\n");
+    *continuar = false;
+  }
 }
 
 unsigned char proximo(unsigned char *a, unsigned int i, unsigned int j,
                       unsigned char regla, unsigned int N){
-	// TODO: falta implementar y pasar a assembler
-	// pongo que devuelva cualquier cosa para probar
-	if ((i+j)%2==0)
-		return 1;
-	return 0;
+  unsigned char izq,der,actual;
+  if (j== 0)
+    izq = *(a+N*i+N);
+  else
+    izq = *(a+N*i+ (j-1) );
+  if (j == N)
+    der = *(a+N*i);
+  else
+    der = *(a+N*i+ (j+1) );
+  actual = *(a+N*i+j);
+  unsigned char pos = 4*izq + 2*actual + der;
+  if ((regla) & (1<<(pos)))
+    return 1;
+  return 0;
 }
 
-void calcularFilas(bool continuar, int N, int regla, unsigned char** matriz) {
-	int i;
-	int j;
-	if (continuar == true) {
-		for (i = 0; i < N - 1; i++) {
-			for (j = 0; j < N; j++) {
-				matriz[i + 1][j] = proximo(&matriz[0][0], i, j, regla, N);
-			}
-		}
-	}
+void calcularFilas(bool continuar, int N, int regla, unsigned char(*matriz)[N]) {
+  int i;
+  int j;
+  if (continuar == true) {
+    for (i = 0; i < N - 1; i++) {
+      for (j = 0; j < N; j++) {
+        matriz[i + 1][j] = proximo(&matriz[0][0], i, j, regla, N);
+      }
+    }
+  }
 }
 
-FILE* escribirImagen(bool continuar, int* N, unsigned char** matriz) {
-	int i;
-	int j;
-	FILE* salida;
-	// Invierto el orden de los if para que pruebe primero de abrirlo asi despues al cerrarlo no hay segfault
-	if (((salida = fopen("salida.pmb", "wb")) == NULL) && (continuar == true)) {
-		fprintf(stderr, "Error al crear archivo salida.\n");
-		continuar = false;
-	}
-	if (continuar == true) {
-		fprintf(salida, "P1\n%d %d\n", *N, *N);
-		for (i = 0; i < *N; i++) {
-			for (j = 0; j < *N; j++) {
-				fprintf(salida, "%d", matriz[i][j]);
-			}
-			fputc('\n', salida);
-		}
-	}
-	return salida;
+FILE* escribirImagen(bool continuar, int N, unsigned char (*matriz)[N]) {
+  int i;
+  int j;
+  FILE* salida;
+  // Invierto el orden de los if para que pruebe primero de abrirlo asi despues al cerrarlo no hay segfault
+  if (((salida = fopen("salida.pmb", "wb")) == NULL) && (continuar == true)) {
+    fprintf(stderr, "Error al crear archivo salida.\n");
+    continuar = false;
+  }
+  if (continuar == true) {
+    fprintf(salida, "P1\n%d %d\n", N, N);
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        fprintf(salida, "%d", matriz[i][j]);
+      }
+      fputc('\n', salida);
+    }
+  }
+  return salida;
 }
 
-void liberarMemoriaYCerrarArchivos(int N, unsigned char** matriz, FILE* entrada, FILE* salida) {
-	int i;
-	for (i = 0; i < N; i++)
-		free(matriz[i]);
-	free(matriz);
-	fclose(entrada);
-	fclose(salida);
+void liberarMemoriaYCerrarArchivos(int N, unsigned char (*matriz)[N], FILE* entrada, FILE* salida) {
+  free(matriz);
+  fclose(entrada);
+  fclose(salida);
 }
 
 int main(int argc, char *argv[]) {
-	//TODO: Verificar que haya algo (y que sea correcto) en argv
-	// En toda esta primera parte donde todavia no se pidio memoria dejo los return -1 que es mas comodo.
-	int regla = verificarRegla(argv[1]);
-	if (regla == -1) return -1;
+  //TODO: Verificar que haya algo (y que sea correcto) en argv
+  // En toda esta primera parte donde todavia no se pidio memoria dejo los return -1 que es mas comodo.
+  int regla = verificarRegla(argv[1]);
+  if (regla == -1) return -1;
 
-	int N = verificarN(argv[2]);
-	if (N == -1) return -1;
+  int N = verificarN(argv[2]);
+  if (N == -1) return -1;
 
-	FILE* entrada = verificarEntrada(argv[3]);
-	if (entrada == NULL) return -1;
+  FILE* entrada = verificarEntrada(argv[3]);
+  if (entrada == NULL) return -1;
 
+  bool continuar = true;
 
-	unsigned char** matriz = crearMatriz(N);
+  unsigned char (*matriz)[N] = malloc(sizeof *matriz * N);
+  if (!matriz)
+    continuar = false;
 
-	bool continuar = true;
-	cargarPrimeraFila(N, &continuar, entrada, matriz);
+  cargarPrimeraFila(N, &continuar, entrada, matriz);
 
-	calcularFilas(continuar, N, regla, matriz);
+  calcularFilas(continuar, N, regla, matriz);
 
-	FILE* salida = escribirImagen(continuar, &N, matriz);
+  FILE* salida = escribirImagen(continuar, N, matriz);
 
-	liberarMemoriaYCerrarArchivos(N, matriz, entrada, salida);
+  liberarMemoriaYCerrarArchivos(N, matriz, entrada, salida);
 
-	fprintf(stdout, "The End.\n");
-	return 0;
+  fprintf(stdout, "The End.\n");
+  return 0;
 }
