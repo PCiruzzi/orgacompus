82cc2756c6afe420a8f0111d6ee09bbc45e942a3
horaciomk <hmk142@hotmail.com>
2015-09-17 12:47:07 -0300

    arreglado lo de valgrind

--- src/main.c
+++ src/main.c
@@ -79,10 +79,10 @@ unsigned char proximo(unsigned char *a, unsigned int i, unsigned int j,
                       unsigned char regla, unsigned int N){
   unsigned char izq,der,actual;
   if (j== 0)
-    izq = *(a+N*i+N);
+    izq = *(a+N*i+ (N-1));
   else
     izq = *(a+N*i+ (j-1) );
-  if (j == N)
+  if (j == (N-1))
     der = *(a+N*i);
   else
     der = *(a+N*i+ (j+1) );
