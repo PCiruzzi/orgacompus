1005ca18d70834239e5da132714062c713314a3c
horacio <hmk142@hotmail.com>
2015-11-23 17:34:27 -0300

    punto 3

---
+++ tp2/punto 3/prueba.asm
@@ -0,0 +1,7 @@
+li $t0,20 #direccion en bytes de la sexta instruccion
+li $t1,1
+jr $t0
+li $t2,2
+li $t3,3
+li $t4,4
+#al finalizar el programa deberian quedar t1=1, t4=4 y t2 y t3 no deberian ser modificados
