7ab15370d1d38a4e3136df504188200e2cbd61f6
horacio <hmk142@hotmail.com>
2015-11-25 19:08:09 -0300

    mas informe

--- tp2/punto 1/prueba5.asm
+++
@@ -1,6 +0,0 @@
-li $t1,32767
-li $t2,17
-sll $t3,$t1,$t2
-li $t2,17
-srl $t3,$t3,$t2
-# 16385 << 17 = -131072 (Recordar complemento a la base), luego -131072 >> 17 = 16385 (No hace extension de signo)
