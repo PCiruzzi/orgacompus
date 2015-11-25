8a4de155732aeb7a16816ad3a5d5fab664fd4e58
Pablo <p.ciruzzi@hotmail.com>
2015-11-25 04:00:55 -0300

    Ahora si, hace un flush cuando hay un jump, evitando que se ejecute la operacion siguiente. Ademas, probe el branch y sigue funcionando, asique creo que no rompi nada. Con esto y alguna que otra pruebita mas, estaria hecho el punto 2. Ademas agrego una pavada en el informe, como para ir arrancandolo de a poco.

--- tp2/punto 2/prueba.asm
+++ tp2/punto 2/prueba.asm
@@ -1,7 +1,9 @@
 	li $t1,1
 	j hola
 	li $t2,2
+	li $t5,5
+	li $t6,6
 hola:
 	li $t3,3
 	li $t4,4
-	#al finalizar el programa deberian quedar t1=1, t3=3, t4=4 y t2 no deberia ser modificado
+	#al finalizar el programa deberian quedar t1=1, t3=3, t4=4 y t2, t5 y t6 no deberian ser modificados
